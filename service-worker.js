436fb7ba8eeabcbe37e31ce48a1fa646ee63ca4e
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2017-07-30 15:39:10 +0530

    implementing pwa

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,30 @@
-var cacheName = 'KarthikPWA';
+var cacheName = 'PWADemo-v1';
 var filesToCache = [
     '/index.html',
-    'css/app.css',
-    'js/app.js'
+    '/css/app.css',
+    '/js/app.js',
+    /* ...and other assets (jQuery, Materialize, fonts, etc) */
 ];
 
-self.addEventListener('install', function(e){
-   console.log('Service Worker Install');
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            console.log('caching');
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
+        })
+    );
+});
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
         })
     );
 });
